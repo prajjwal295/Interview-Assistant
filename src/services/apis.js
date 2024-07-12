86737069d537abb81a5c007d380279edfbe80ca7
prajjwal <mishraprajjwal295@gmail.com>
2024-07-12 13:47:40 +0530

    ..

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,8 +1,8 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const endpoints = {
-  SIGNUP_API: `${BASE_URL}/api/v1/auth/signup`,
-  LOGIN_API: `${BASE_URL}/api/v1/auth/login`,
-  CREATE_FORM_API: `${BASE_URL}/api/v1/auth/createForm`,
-  FETCH_FORM_API: `${BASE_URL}/api/v1/auth/getAllForms`,
+  SIGNUP_API: `${BASE_URL}api/v1/auth/signup`,
+  LOGIN_API: `${BASE_URL}api/v1/auth/login`,
+  CREATE_FORM_API: `${BASE_URL}api/v1/auth/createForm`,
+  FETCH_FORM_API: `${BASE_URL}api/v1/auth/getAllForms`,
 };
